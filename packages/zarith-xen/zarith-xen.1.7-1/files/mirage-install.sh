402ddda86ce1771f6369163b650fe5b7be09bde3
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-01-03 14:05:16 +0100

    add zarith-xen.1.7-1

---
+++ packages/zarith-xen/zarith-xen.1.7-1/files/mirage-install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -eux
+
+PREFIX=`opam config var prefix`
+PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
+export PKG_CONFIG_PATH
+
+LDFLAGS=`pkg-config --libs gmp-xen`
+export LDFLAGS
+
+# WARNING: if you pass invalid cflags here, zarith will silently
+# fall back to compiling with the default flags instead!
+CFLAGS="`pkg-config --cflags gmp-xen mirage-xen-posix` -O2 -pedantic -fomit-frame-pointer -fno-builtin"
+export CFLAGS
+OCAMLFLAGS="-no-keep-locs"
+export OCAMLFLAGS
+OCAMLOPTFLAGS="-no-keep-locs"
+export OCAMLOPTFLAGS
+./configure
+make
+
+cp libzarith.a "$PREFIX/lib/zarith/libzarith-xen.a"
+cp zarith.cma  "$PREFIX/lib/zarith/zarith-xen.cma"
+cp zarith.cmxa "$PREFIX/lib/zarith/zarith-xen.cmxa"
+cp zarith.a    "$PREFIX/lib/zarith/zarith-xen.a"
+
+patch -d $PREFIX/lib/zarith < meta.patch
